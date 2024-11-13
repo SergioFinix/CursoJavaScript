e3cc462a1db47300e86c9583954127ce6d38ca6d
Saul Ricaño Monroy <saulrimso@gmail.com>
2024-11-13 10:03:17 -0600

    12/11-> Rederizado automatico

--- EstructuraDeDatos/Arrays/main.js
+++ EstructuraDeDatos/Arrays/main.js
@@ -6,8 +6,9 @@ const aplicarFormulario = document.getElementById("aplicarFormularios")
 const agregarPregunta = document.getElementById("agregarPregunta")
 const selectForm = document.getElementById("selectForm")
 const formularioLista = document.getElementById("formularioLista")
+// Variables de selección
 
-let formulario = [
+let formulario = [ // Formularios creados 3
     {
         idForm: 1,
         preguntas: []
@@ -22,11 +23,11 @@ let formulario = [
     }
 ]
 
-let pregunta = []
+let pregunta = [] // Preguntas creadas
 
-let usuarios = []
+let usuarios = [] // Usuarios agregados
 
-let respuestas = [
+let respuestas = [ //
     {
         idUsuario: 1,
         idPregunta: 1,
@@ -34,43 +35,46 @@ let respuestas = [
     }
 ]
 
-function agregarUsuarios(nombre) {
+function agregarUsuarios(nombre) { // Agrega un nuevo usuario
+    if(!nombre.trim()) return
     const idArray = usuarios.length + 1
     usuarios.push({ "id": idArray, "nombre": nombre })
 }
-function eliminarUsuarios() {
+function eliminarUsuarios() { // Elimina el ultimo usuario
     usuarios.pop()
 }
-function respuestaAleatoria(idPregunta) {
+function respuestaAleatoria(idPregunta) { // genera una respuesta int según el num de opciones
     const elemento = pregunta.find(item => item.idPregunta == idPregunta)
     const num = elemento.opcion.length
-    return Math.floor((Math.random() * num))+1
+    return Math.floor((Math.random() * num)) + 1
 }
-function aplicarFormularios() {
+function aplicarFormularios() { // Recorre todos los formularios para calcular
+    console.log("-------------")
     formulario.forEach(form => { // Todos los formularios
         console.log("formulario: " + form.idForm)
-        form.preguntas.forEach(elementPregunta=> { // Preguntas
+        form.preguntas.forEach(elementPregunta => { // Preguntas
             usuarios.forEach(usu => {
-                console.log(form.idForm + "- " + usu.nombre +": "+ respuestaAleatoria(elementPregunta))
+                console.log(form.idForm + "- " + usu.nombre + ": " + respuestaAleatoria(elementPregunta))
+                // Guardar las respuestas aquí
             })
 
         })
     })
 }
 
-function agregarPreguntas(numFormulario) {
-    let id
-    formulario.forEach(item =>{
-        if(item.idForm == numFormulario){
+function agregarPreguntas(numFormulario, preguntaData) { // Agrega una nueva pregunta
+    let bodyText = ""
+    formulario.forEach(item => {
+        if (item.idForm == numFormulario) {
             item.preguntas.push(pregunta.length + 1)
             id = item.preguntas.length
         }
     })
-    
 
-    const obj =
+
+    const obj = // Remplaza el objData
     {
-        idPregunta: pregunta.length+1,
+        idPregunta: pregunta.length + 1,
         texto: "terminado?",
         opcion: [
             {
@@ -88,36 +92,52 @@ function agregarPreguntas(numFormulario) {
             }
         ]
     }
-    
+
+    bodyText += `<h2>${obj.texto}</h2><select>`
+    obj.opcion.forEach(item =>{
+        bodyText += `<option>${item.texto}</option>`
+    })
+    bodyText += `</select>`
+    formularioLista.innerHTML += bodyText
     pregunta.push(obj)
-    console.log(pregunta)
 
 }
 
-function renderizarFormulario(id){
+function renderizarFormulario(id) { // Agrega los elementos de Form a la lista
     formularioLista.innerHTML = ""
     const form = formulario.find(item => item.idForm == id)
-    let j = 0
-    for (let i = 0; i < form.length; i++) {
-        while(j < pregunta.length){
-            console.log(pregunta[j] == form[i])
-            j++
-        }
-    }
+    let aux
+    let bodyText = ""
+    form.preguntas.forEach(formPregunta => {
+        aux = pregunta.find(item => item.idPregunta == formPregunta)
+        bodyText += `<h2>${aux.texto}</h2><ul>`
+        aux.opcion.forEach(opcion => {
+            bodyText += `
+                    <li>
+                        ${opcion.texto}
+                    </li>
+                `
 
-    
-    
+        })
+        bodyText += "</ul>"
+        formularioLista.innerHTML = bodyText
+    })
+
+
+
+
 }
 
+// Disparadores de los botones
 agregarUsuario.addEventListener('click', () => {
     const nombre = usuarioInput.value
     agregarUsuarios(nombre)
-    mostrarUsuarios()
+    renderizarUsuario()
 })
 
 eliminarUsuario.addEventListener('click', () => {
     eliminarUsuarios()
-    mostrarUsuarios()
+    renderizarUsuario()
 })
 
 aplicarFormulario.addEventListener('click', () => {
@@ -128,11 +148,11 @@ agregarPregunta.addEventListener('click', () => {
     agregarPreguntas(selectForm.value)
 })
 
-selectForm.addEventListener('change', () =>{
+selectForm.addEventListener('change', () => {
     renderizarFormulario(selectForm.value)
 })
 
-function mostrarUsuarios() {
+function renderizarUsuario() {
     usuariosLista.innerHTML = ""
     usuarios.forEach(usu => {
         usuariosLista.innerHTML += `<li>${usu.nombre}</li>`
