666b6018e23a354d6d2b74fbbb02fc94c60d5880
JuanAMG <juanadrianmurillogonzalez59@gmail.com>
2024-11-15 17:24:43 -0800

    agrege una funcion para ingresar datos en la tabla del mes 3

--- Proyecto/funciones.js
+++ Proyecto/funciones.js
@@ -26,16 +26,30 @@ function personas(){
 //Muestra para mostrar la tabla de productos y agregar mas de la Primera Persona
 function cal1(){
     console.log("llega aqui")
-    let calculo =20+20+45+26;
-    let tabla="";
-    console.log("calculo "+calculo)
-    tabla+=`<tr> <td> TOTAL </td>       <td>----</td>       <td>---</td><td> ${calculo}</td> </tr>`
-    tabla+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
-    tabla+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
-    tabla+=`<tr> <td> AZUCAR </td>      <td> 45 </td>       <td> 1 </td><td> 45 </td> </tr>`
-    tabla+=`<tr> <td> LECHE </td>       <td> 23 </td>       <td> 2 </td><td> 26 </td> </tr>`
+    let calculo1 =30+20+45+26;
+    let tabla1="";
+    console.log("calculo "+calculo1)
+    tabla1+=`<tr> <td> TOTAL </td>       <td>----</td>       <td>---</td><td> ${calculo1}</td> </tr>`
+    tabla1+=`<tr> <td> AGUA </td>        <td> 15 </td>       <td> 2 </td><td> 30 </td> </tr>`
+    tabla1+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
+    tabla1+=`<tr> <td> AZUCAR </td>      <td> 45 </td>       <td> 1 </td><td> 45 </td> </tr>`
+    tabla1+=`<tr> <td> LECHE </td>       <td> 23 </td>       <td> 2 </td><td> 26 </td> </tr>`
 
-    document.getElementById("mes1").innerHTML = tabla
+    document.getElementById("mes1").innerHTML = tabla1
+
+    console.log("mes numero 2")
+    let calculo2 =20+20+45+26;
+    let tabla2="";
+    console.log("calculo "+calculo2)
+    tabla2+=`<tr> <td> TOTAL </td>       <td>----</td>       <td>---</td><td> ${calculo2}</td> </tr>`
+    tabla2+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
+    tabla2+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
+    tabla2+=`<tr> <td> AZUCAR </td>      <td> 45 </td>       <td> 1 </td><td> 45 </td> </tr>`
+    tabla2+=`<tr> <td> LECHE </td>       <td> 23 </td>       <td> 2 </td><td> 26 </td> </tr>`
+
+    document.getElementById("mes2").innerHTML = tabla2
+
+
 }
 
 
@@ -50,3 +64,33 @@ function cal2(){
     document.getElementById("mes1").innerHTML = tabla
 }
 
+
+//agregar elementos a la 3er tabla
+
+    function agregar(){
+        const tabla = document.getElementById("mes3").getElementsByTagName("tbody")[0];
+        
+        event.preventDefault();
+        let nom = document.getElementById("nombre").value;
+        let pre = document.getElementById("precio").value;
+        let canti = document.getElementById("cantidad").value;
+        console.log(nom);
+        console.log(pre);
+        console.log(canti);
+            
+        let filanu = document.createElement("tr");
+        let productoing = document.createElement("td");
+        let precioing = document.createElement("td");
+        let cantidading = document.createElement("td");
+
+        productoing.textContent = `${nom}`;
+        precioing.textContent = `${pre}`;
+        cantidading.textContent = `${canti}`;
+
+        filanu.appendChild(productoing);
+        filanu.appendChild(precioing);
+        filanu.appendChild(cantidading);
+
+        tabla.appendChild(filanu);
+            
+    }
