2f327dab70bb51f769f4c04209378510fd8dbf83
Saul Ricaño Monroy <saulrimso@gmail.com>
2024-11-19 17:59:36 -0600

    Modificación | Promesa

--- EstructuraDeDatos/Arrays/main.js
+++ EstructuraDeDatos/Arrays/main.js
@@ -8,24 +8,27 @@ const grafica1 = document.getElementById("chart_div1")
 const grafica2 = document.getElementById("chart_div2")
 const grafica3 = document.getElementById("chart_div3")
 const nameTitle = document.getElementById("nameTitle")
+// Constantes de selección del DOM
 
 let numFormulario = 1;
 let usuarioSelect = 1;
-let respuestasUsuario = []
+let respuestasUsuario = [] // Respuestas de los formularios por usuarios
 
+// Conteo de los formularios para las graficas
 let respuestasForm1 = [
     ['Gatos o Perros', 'Total',],
-    [0,0]
+    [0, 0]
 ]
 let respuestasForm2 = [
     ['Introvertido o Extrovertido', 'Total',],
-    [0,0]
+    [0, 0]
 ]
 let respuestasForm3 = [
     ['Frío o calor', 'Total',],
-    [0,0]
+    [0, 0]
 ]
 
+// Petición a la api, información asyncrona.
 async function peticionUsuario() {
     const url = "https://jsonplaceholder.typicode.com/users"
     try {
@@ -45,12 +48,16 @@ async function peticionUsuario() {
     }
 
 }
+
+// Seleccionar de la lista de usuarios cual observar
 function seleccionarUsuario(id, name) {
     usuarioSelect = id
     nameTitle.innerText = name
     renderRespuestas(id)
 }
 
+// Muestra las respuestas guardadas en los componentes de formulario
+// Calcular las estadisticas de las respuestas guardadas para observar en graficación
 function renderRespuestas() {
     const res = respuestasUsuario.find(item => item.idUsuario === usuarioSelect)
     const formSelect1 = document.querySelectorAll("#form1 .selectForm")
@@ -64,18 +71,19 @@ function renderRespuestas() {
             formSelect3[i].value = res.respuestas[2].formulario[i].option
         }
         renderGraficas()
-    } else {
-        const valorSelect = [1,2,1,1,2,1,1,2,1]
+    } else { // En caso de no existir respuestas Limpia el formulario
+        const valorSelect = [1, 2, 1, 1, 2, 1, 1, 2, 1]
         for (let i = 0; i < formSelect1.length; i++) {
             formSelect1[i].value = valorSelect[i]
             formSelect2[i].value = valorSelect[i]
             formSelect3[i].value = valorSelect[i]
         }
-        renderGraficas(1)
+        renderGraficas(1) // Evita la graficación, limpia
     }
 
 }
 
+// Obteniendo un array de la API, muestra los usuarios en pantalla con el metodo para seleccionar
 function renderUsarios(data) {
     let bodyComponent = ""
     nameTitle.innerText = data[0].username
@@ -86,18 +94,17 @@ function renderUsarios(data) {
     listUser.innerHTML = bodyComponent
 }
 
-async function init() {
-    const data = await peticionUsuario()
-    renderUsarios(data)
-    data.forEach(item => {
-        respuestasUsuario.push({
-            idUsuario: item.id,
-            respuestas: {}
-        })
-    })
+
+function tiempoEspera(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`He esperado 1`);
+        }, 500);
+    });
 }
 
-saveForm.addEventListener('click', () => {
+// Guarda las respuestas
+saveForm.addEventListener('click', async () => {
     let i = 1
     let opcion = ""
     let respuestas = [
@@ -120,72 +127,49 @@ saveForm.addEventListener('click', () => {
             ]
         }
     ]
-    let uno = 0
-    let dos = 0
+    // estructura temporal de respuestas
+    await tiempoEspera()
+
     const preguntasFormText = document.querySelectorAll(".questionForm")
     document.querySelectorAll("#form1 div select").forEach(item => {
         opcion = item.options[item.selectedIndex].text
         respuestas[0].formulario.push(resForm(i, opcion, preguntasFormText[i].textContent, item.value))
-        if(item.value == 1) {
-            uno++
-        } else{
-            dos++
-        }
+
         i++
     })
-    respuestasForm1 = [
-        ['Gatos o Perros', 'Puntos',],
-        ['Perros', uno],
-        ['Gatos', dos],
-    ]
+
     uno = dos = i = 0
 
     document.querySelectorAll("#form2 div select").forEach(item => {
         opcion = item.options[item.selectedIndex].text
         respuestas[1].formulario.push(resForm(i, opcion, preguntasFormText[i].textContent, item.value))
-        if(item.value == 1) {
-            uno++
-        } else{
-            dos++
-        }
+
         i++
     })
-    respuestasForm2 = [
-        ['Introvertido o Extrovertido', 'Puntos',],
-        ['Extrovertido', uno],
-        ['Introvertido', dos],
-    ]
+
     uno = dos = i = 0
 
     document.querySelectorAll("#form3 div select").forEach(item => {
         opcion = item.options[item.selectedIndex].text
         respuestas[2].formulario.push(resForm(i, opcion, preguntasFormText[i].textContent, item.value))
-        if(item.value == 1) {
-            uno++
-        } else{
-            dos++
-        }
+
         i++
-        console.log(uno,dos)
+ 
     })
-    respuestasForm3 = [
-        ['Frío o calor', 'Puntos',],
-        ['Frío', uno],
-        ['Calor', dos],
-    ]
-    uno, dos, i = 0
+
+    uno = dos = i = 0
     const itemFind = respuestasUsuario.find(item => item.idUsuario === usuarioSelect)
     itemFind.respuestas = respuestas
-    drawBasic()
-    //alert("Se han guardado los tres formulario")
+    renderGraficas() // Renderiza una vez guardadas las respuestas
 })
 
-function renderGraficas(vacio){
+// calcula los datos de las graficas y luego las dibuja
+function renderGraficas(vacio) {
     let i = 1
     let uno = 0
     let dos = 0
 
-    if(vacio) {
+    if (vacio) {
         respuestasForm1 = [
             ['Gatos o Perros', 'Puntos',],
             ['Perros', uno],
@@ -206,9 +190,9 @@ function renderGraficas(vacio){
     }
 
     document.querySelectorAll("#form1 div select").forEach(item => {
-        if(item.value == 1) {
+        if (item.value == 1) {
             uno++
-        } else{
+        } else {
             dos++
         }
         i++
@@ -221,9 +205,9 @@ function renderGraficas(vacio){
     uno = dos = i = 0
 
     document.querySelectorAll("#form2 div select").forEach(item => {
-        if(item.value == 1) {
+        if (item.value == 1) {
             uno++
-        } else{
+        } else {
             dos++
         }
         i++
@@ -236,9 +220,9 @@ function renderGraficas(vacio){
     ]
     uno = dos = i = 0
     document.querySelectorAll("#form3 div select").forEach(item => {
-        if(item.value == 1) {
+        if (item.value == 1) {
             uno++
-        } else{
+        } else {
             dos++
         }
         i++
@@ -249,12 +233,12 @@ function renderGraficas(vacio){
         ['Frío', uno],
         ['Calor', dos],
     ]
-    console.log(uno,dos)
+    console.log(uno, dos)
     uno = dos = i = 0
     drawBasic()
 }
 
-
+// Segun un boton, oculta o muestra los formularios
 function mostrarForm(key) {
     if (key <= 0 || key > 3) return
     numFormulario = key
@@ -282,12 +266,12 @@ function mostrarForm(key) {
     }
 }
 
+// ---------- Google charts ----------------------
 google.charts.load('current', { packages: ['corechart', 'bar'] });
 google.charts.setOnLoadCallback(drawBasic);
 
-
+// Dibuja los graficos
 function drawBasic() {
-
     var data1 = google.visualization.arrayToDataTable(respuestasForm1);
     var data2 = google.visualization.arrayToDataTable(respuestasForm2);
     var data3 = google.visualization.arrayToDataTable(respuestasForm3);
@@ -295,17 +279,17 @@ function drawBasic() {
     var options1 = {
         title: 'Gatos o Perros',
         chartArea: { width: '50%' },
-        colors: ['#b972d4'] 
+        colors: ['#b972d4']
     };
     var options2 = {
-        title: 'Introvertido p Extrovertido',
+        title: 'Introvertido o Extrovertido',
         chartArea: { width: '50%' },
-        colors: ['#b972d4'] 
+        colors: ['#b972d4']
     };
     var options3 = {
         title: 'Dulce o Amargo',
         chartArea: { width: '50%' },
-        colors: ['#b972d4'] 
+        colors: ['#b972d4']
     };
 
     var chart = new google.visualization.BarChart(grafica1);
@@ -316,6 +300,7 @@ function drawBasic() {
     chart.draw(data3, options3);
 }
 
+// Estructura de la respuesta para guardar en el obj de respuestas (Por usuario)
 function resForm(id, pregunta, opcion, valor) {
     return {
         idQuestion: id,
@@ -325,6 +310,19 @@ function resForm(id, pregunta, opcion, valor) {
     }
 }
 
+// Metodo de iniciación
+async function init() {
+    const data = await peticionUsuario()
+    renderUsarios(data)
+    data.forEach(item => {
+        respuestasUsuario.push({
+            idUsuario: item.id,
+            respuestas: {}
+        })
+    })
+}
+
+// Inicica la aplicación una vez que se haya cargado la pagina
 window.addEventListener('load', () => {
     init()
     mostrarForm(1)
