5361c880c093cd09a4d30e0ca5ad4361077245b3
JuanAMG <juanadrianmurillogonzalez59@gmail.com>
2024-11-18 22:36:24 -0800

    agrege un nuevo html en el que coloque un boton y un promise en donde terminado el tiempo redirecciona la pagina y modifique el array

--- Proyecto/funciones.js
+++ Proyecto/funciones.js
@@ -13,14 +13,20 @@ function personas(){
         let tabla=``;
         let fun="";
         let i =0, c=1;
-        for(i;i<10;i++){
+        for(i;i<3;i++){
             fun= "cal"+c+"()";
             tabla+=`<tr> <td> ${c} </td> <td> ${data[i].name} </td> <td> <button onclick="${fun}"> Calculadora</button> </td> </tr>`
             c++;
+            console.log(c)
+            console.log(fun)
         }
         document.getElementById("nom").innerHTML = tabla
     }
 
+}
+
+function presupuesto(){
+    let
 }
 
 //Muestra para mostrar la tabla de productos y agregar mas de la Primera Persona
@@ -42,14 +48,12 @@ function cal1(){
     let tabla2="";
     console.log("calculo "+calculo2)
     tabla2+=`<tr> <td> TOTAL </td>       <td>----</td>       <td>---</td><td> ${calculo2}</td> </tr>`
-    tabla2+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
+    tabla2+=`<tr> <td> AGUA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
     tabla2+=`<tr> <td> SOPA </td>        <td> 10 </td>       <td> 2 </td><td> 20 </td> </tr>`
     tabla2+=`<tr> <td> AZUCAR </td>      <td> 45 </td>       <td> 1 </td><td> 45 </td> </tr>`
     tabla2+=`<tr> <td> LECHE </td>       <td> 23 </td>       <td> 2 </td><td> 26 </td> </tr>`
 
     document.getElementById("mes2").innerHTML = tabla2
-
-
 }
 
 
@@ -65,16 +69,29 @@ function cal2(){
 }
 
 
-//agregar elementos a la 3er tabla
+//agregar elementos a la 3er tablase agrego un array para almacenar los datos
+let productoscom=[];
 
     function agregar(){
+
+        let presupuesto;
+        let tama;
+
         const tabla = document.getElementById("mes3").getElementsByTagName("tbody")[0];
         
         event.preventDefault();
-        let nom = document.getElementById("nombre").value;
+
+        let nom = document.getElementById("produc").value;
+        let nombre = nom.toUpperCase();
         let pre = document.getElementById("precio").value;
         let canti = document.getElementById("cantidad").value;
+        let total = pre*canti;
+        tama=  `[${nombre},${pre},${canti},${total}]`;
+
+        productoscom.push(tama);
+
         console.log(nom);
+        console.log(nombre);
         console.log(pre);
         console.log(canti);
             
@@ -82,15 +99,18 @@ function cal2(){
         let productoing = document.createElement("td");
         let precioing = document.createElement("td");
         let cantidading = document.createElement("td");
+        let totaling = document.createElement("td");
 
-        productoing.textContent = `${nom}`;
+        productoing.textContent = `${nombre}`;
         precioing.textContent = `${pre}`;
         cantidading.textContent = `${canti}`;
+        totaling.textContent = `${total}`;
 
         filanu.appendChild(productoing);
         filanu.appendChild(precioing);
         filanu.appendChild(cantidading);
+        filanu.appendChild(totaling);
 
         tabla.appendChild(filanu);
-            
+        console.log(productoscom)
     }
